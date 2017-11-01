18f0d5076d70e8e31732392fd41c51ff0bcfacdc
ruoxuan.fu <ruoxuan.fu@wheatek.com>
2017-11-01 14:36:19 +0800

    1

--- app/src/main/java/com/example/ruoxuanfu/myapplication/LoadCallBack.java
+++ app/src/main/java/com/example/ruoxuanfu/myapplication/LoadCallBack.java
@@ -1,9 +1,5 @@
 package com.example.ruoxuanfu.myapplication;
 
-/**
- * Created by yue.cui on 2017/10/13.
- */
-
 public interface LoadCallBack {
     void LoadSuccessCallBack();
     void LoadErrorCallBack();
