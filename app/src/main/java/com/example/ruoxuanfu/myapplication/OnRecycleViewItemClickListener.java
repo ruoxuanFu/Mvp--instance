97795168be39f85e9e43dfb95abd4731ce91c796
ruoxuan.fu <ruoxuan.fu@wheatek.com>
2017-11-01 14:37:12 +0800

    1

--- app/src/main/java/com/example/ruoxuanfu/myapplication/OnRecycleViewItemClickListener.java
+++ app/src/main/java/com/example/ruoxuanfu/myapplication/OnRecycleViewItemClickListener.java
@@ -1,9 +1,5 @@
 package com.example.ruoxuanfu.myapplication;
 
-/**
- * Created by yue.cui on 2017/10/13.
- */
-
 public interface OnRecycleViewItemClickListener {
     void onItemClickListener(int position);
 }
